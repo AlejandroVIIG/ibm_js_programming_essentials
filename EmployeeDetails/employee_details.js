e37e284f59543df93f044bb64c0779e68f138550
Alejandro Villavicencio <alejandro.viig@gmail.com>
2024-08-19 16:47:21 -0400

    employee details 2nd commit -m

--- EmployeeDetails/employee_details.js
+++ EmployeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', specialization: 'Javascript', salary: 50000 },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', specialization: 'Python', salary: 45000 },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', specialization: 'Java', salary: 60000 },
     //... More employee records can be added here
 ];
 
@@ -39,4 +39,16 @@ function findEmployeeById(employeeId) {
     } else{
         document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
     }
+}
+
+function displayJavascriptEmployees(specialization) {
+    const jsEmployee =
+        employees.find(employee => employee.specialization === specialization);
+    if(!jsEmployee) {
+        document.getElementById('employeesDetails').innerHTML =
+            'no employee has been found with this specialization';
+        return
+    }
+    document.getElementById('employeesDetails').innerHTML =
+    `<p>${jsEmployee.id}: ${jsEmployee.name}: ${jsEmployee.age} - ${jsEmployee.department} - $${jsEmployee.salary}</p>`;
 }
